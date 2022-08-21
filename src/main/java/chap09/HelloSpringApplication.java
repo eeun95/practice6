063ec39280939bafcefa91c28d57d7e4b201ca5e
eeun <eeun95@gmail.com>
2022-08-21 18:01:25 +0900

    스프링 MVC 시작하기

--- src/main/java/chap09/HelloSpringApplication.java
+++ src/main/java/chap09/HelloSpringApplication.java
@@ -3,6 +3,9 @@ package chap09;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 스프링 부트 프로젝트 구조가 변경이 일어났기 때문에 스프링 부트에서 사용하는 뷰 리졸버(ViewResolver)의 설정 값을 변경 (정확히는 ViewResolver의 경로(Path)를 수정해야 함)
+// application.properties 에 변경해줬음 (톰캣 깔기 싫어서 스프링 부트로 ........)
+
 @SpringBootApplication
 public class HelloSpringApplication {
 
